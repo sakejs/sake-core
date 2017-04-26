859545bf120f6ac0b76292337e1ecb089fc39b2a
zeekay <z@zeekay.io>
2017-04-26 09:29:52 -0500

    Add TypeScript declaration file

---
+++ sake.d.ts
@@ -0,0 +1,3 @@
+declare var exec: (command: string) => void
+declare var task: (command: string, description: string, dependency?: string[], fn?: any) => void
+declare var use:  (plugin:  string) => void
