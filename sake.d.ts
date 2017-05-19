c69c3e18de27d1215c595062c24fa8d7dfc4ecc8
zeekay <z@zeekay.io>
2017-05-19 08:44:58 -0500

    Reformat

--- sake.d.ts
+++ sake.d.ts
@@ -1,6 +1,6 @@
 declare var exec:    (command: string | any[], options?: any, callback?: any) => void
 declare var invoke:  (command: string | string[]) => void
-declare var require: (module: string)  => any
+declare var require: (module: string) => any
 declare var task:    (command: string, description: string, ...args: any[]) => void
 declare var use:     (plugin:  string) => void
 declare var watch:   (path: string, fn: any) => void
