bb021daf270d95b90a651bfdfe73730082777f19
zeekay <z@zeekay.io>
2017-05-21 15:20:02 -0500

    Update sake types

--- sake.d.ts
+++ sake.d.ts
@@ -1,5 +1,5 @@
-declare var exec:    (command: string | any[], options?: any, callback?: any) => void
-declare var invoke:  (command: string | string[]) => void
+declare var exec:    (command: string | any[], options?: any, callback?: any) => Promise<any>
+declare var invoke:  (command: string | string[]) => Promise<any>
 declare var require: (module: string) => any
 declare var task:    (command: string, description: string, ...args: any[]) => void
 declare var use:     (plugin:  string) => void
