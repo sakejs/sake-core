d3e10b58f1a9ee7d0b70ebd6bd0b36a2dffdf94b
zeekay <z@zeekay.io>
2017-05-21 16:53:33 -0500

    Update types

--- sake.d.ts
+++ sake.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 interface executive {
   (command: string | any[], options?: any, callback?: any): Promise<any>
   interactive: executive
@@ -7,9 +9,9 @@ interface executive {
   strict: executive
   sync: (command: string | any[], options?: any, callback?: any) => any
 }
-declare var exec:    executive
+declare var exec: executive
+
 declare var invoke:  (command: string | string[]) => Promise<any>
-declare var require: (module: string) => any
 declare var task:    (command: string, description: string, ...args: any[]) => void
 declare var use:     (plugin:  string) => void
 declare var watch:   (path: string, fn: any) => void
