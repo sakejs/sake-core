bcce18c09a259f1b65e81eb7d9cad8c4d6b92a5a
zeekay <z@zeekay.io>
2017-05-21 23:08:29 -0500

    Nicer syntax

--- sake-core.d.ts
+++ sake-core.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="node" />
 
-import {executive} from 'executive'
+import executive from 'executive'
 
 declare global {
-  var exec:    executive
-  var invoke:  (command: string | string[]) => Promise<any>
-  var task:    (command: string, description: string, ...args: any[]) => void
-  var use:     (plugin:  string) => void
-  var watch:   (path: string, fn: any) => void
+  var exec:   executive
+  var invoke: (command: string | string[]) => Promise<any>
+  var task:   (command: string, description: string, ...args: any[]) => void
+  var use:    (plugin:  string) => void
+  var watch:  (path: string, fn: any) => void
 }
